7d40e27a5bdca68a4ab93541c0a42fab1cc41c84
Nofy261 <noufy33@yahoo.com>
2026-04-29 11:19:07 +0200

    understanding the project

--- src/predict.py
+++ src/predict.py
@@ -1,2 +1,26 @@
 utilise le modèle entraîné
 fait des prédictions
+calcul de prediction donné dans le sujet 
+price_predict = theta0 + (thetha1 * km)
+
+ecrire un efonction qui utilise la formule pour :
+MAE = erreur (difference entre le vrai prix et le prix estimer)
+
+erreur = prix_estimer - prix_reel
+
+somme de toutes les erreurs divisé par le nombre des voitures = moyenne erreur 
+----------------------------------
+1) train = calculer theta0 et theta1
+2) predict = utliser theta0 et theta1
+-------------------------------
+
+etapes:
+
+lire theta0 et theta1 depuis le fichier
+
+demander à l’utilisateur un mileage
+
+calculer estimation :
+    prix = theta0 + theta1 * mileage
+
+afficher le prix estimé
--- src/train.py
+++ src/train.py
@@ -1,2 +1,44 @@
 entraîne le modèle
 trouve les bons paramètres
+
+tester → mesurer erreur → ajuster → recommencer
+
+1 ) train 
+- calculer theta0 et theta1 
+
+2) predict
+- utiliser theta0 et theta1
+
+---------------------------------
+
+ Etape : gradient descent
+lire le fichier data.csv
+stocker toutes les valeurs mileage et price
+
+initialiser theta0 = 0
+initialiser theta1 = 0
+
+choisir learningRate
+choisir nombre d’itérations
+
+répéter pour chaque itération :
+
+    initialiser somme_erreur = 0
+    initialiser somme_erreur_mileage = 0
+
+    pour chaque donnée i :
+
+        estimation = theta0 + theta1 * mileage[i]
+
+        erreur = estimation - price[i]
+
+        somme_erreur += erreur
+        somme_erreur_mileage += erreur * mileage[i]
+
+    tmp_theta0 = learningRate * (1/m) * somme_erreur
+    tmp_theta1 = learningRate * (1/m) * somme_erreur_mileage
+
+    theta0 = theta0 - tmp_theta0
+    theta1 = theta1 - tmp_theta1
+
+sauvegarder theta0 et theta1
